d80d8d06fbdd69f511be4822bed31d08d66d29d3
filippocarastro <filippo.carastro@icloud.com>
2022-09-09 09:45:01 +0200

    Add rtcclock and test

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -82,40 +82,32 @@ module user_project_wrapper #(
 /* User project is instantiated  here   */
 /*--------------------------------------*/
 
-user_proj_example mprj (
+rtcclock rtcclock(
 `ifdef USE_POWER_PINS
 	.vccd1(vccd1),	// User area 1 1.8V power
 	.vssd1(vssd1),	// User area 1 digital ground
 `endif
 
-    .wb_clk_i(wb_clk_i),
-    .wb_rst_i(wb_rst_i),
-
+    .i_clk(wb_clk_i),
+    .rst(wb_rst_i),
+    
     // MGMT SoC Wishbone Slave
-
-    .wbs_cyc_i(wbs_cyc_i),
-    .wbs_stb_i(wbs_stb_i),
-    .wbs_we_i(wbs_we_i),
-    .wbs_sel_i(wbs_sel_i),
-    .wbs_adr_i(wbs_adr_i),
-    .wbs_dat_i(wbs_dat_i),
-    .wbs_ack_o(wbs_ack_o),
-    .wbs_dat_o(wbs_dat_o),
-
-    // Logic Analyzer
-
-    .la_data_in(la_data_in),
-    .la_data_out(la_data_out),
-    .la_oenb (la_oenb),
+    
+    .i_wb_cyc(wbs_cyc_i),
+    .i_wb_stb(wbs_stb_i),
+    .i_wb_we(wbs_we_i),
+    .i_wb_addr(wbs_adr_i),
+    .i_wb_data(wbs_dat_i),
+    .o_data(wbs_dat_o),
+    
 
     // IO Pads
 
-    .io_in (io_in),
-    .io_out(io_out),
-    .io_oeb(io_oeb),
+    .i_hack(io_in),
+    .o_sseg(io_out[31:0]),
+    .o_led(io_out[46:32]),
+    .io_oeb(io_oeb[15:0])
 
-    // IRQ
-    .irq(user_irq)
 );
 
 endmodule	// user_project_wrapper
